ec5840089338a49456e7f32d2bd414d20b880f1d
joaoteixeira <joaoteixeira@ciandt.com>
2025-08-18 19:01:19 -0300

    feat: refactor code

---
+++ binary-search/binary-search.js
@@ -0,0 +1,25 @@
+function binarySearch(nums, numForSearch){
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+    let founded = nums[middle]
+
+    if(founded === numForSearch){
+      return middle
+    }
+
+    if(middle > numForSearch){
+      right = middle - 1
+    }else if(middle < numForSearch){
+      left = middle + 1
+    }
+  }
+
+  return null
+}
+
+const nums = [1, 2, 3, 7, 8, 9]
+
+console.log("Index of the search: ", binarySearch(nums, 8))
